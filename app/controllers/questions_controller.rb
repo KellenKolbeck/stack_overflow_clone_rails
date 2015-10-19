3db51ae1a0833e6719d20043d5aaafd1a710a248
Kellen Kolbeck & Brigham Thompson <student@epicodus.com>
2015-10-19 12:27:06 -0700

    add index and new path and create function for question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,38 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to questions_path
+    else
+      render :new
+  end
+
+  # def edit
+  #
+  # end
+  #
+  # def update
+  #
+  # end
+  #
+  # def destroy
+  #
+  # end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:question_content)
+  end
 end
