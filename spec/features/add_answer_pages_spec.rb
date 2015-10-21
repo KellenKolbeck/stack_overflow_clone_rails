8d35abc6556ab7b3566d2701849ef6718635e6b9
kellen kolbeck & brigham thompson <student@epicodus.com>
2015-10-20 17:55:52 -0700

    more stuff

--- spec/features/add_answer_pages_spec.rb
+++ spec/features/add_answer_pages_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe "the add answer process" do
   it "adds an answer to a question" do
-
     answer = FactoryGirl.create(:answer)
     login_user
     click_on answer.question.question_content
